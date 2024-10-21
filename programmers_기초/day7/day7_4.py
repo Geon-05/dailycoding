e7016f0af9c7e161362131d4e62cb1f7c48982df
Geon05 <zqrc05@gmail.com>
2024-10-21 13:56:47 +0900

    20241021

--- programmers_기초/day7/day7_4.py
+++ programmers_기초/day7/day7_4.py
@@ -1,1 +1,14 @@
 # day7 콜라츠 수열 만들기
+
+def solution(n):
+    answer = []
+    while True:
+        answer.append(int(n))
+        if n % 2 == 0:
+            n /= 2
+        else:
+            n = 3 * n + 1
+        
+        if answer[-1] == 1:
+            break
+    return answer
--- programmers_기초/day7/day7_5.py
+++ programmers_기초/day7/day7_5.py
@@ -1,1 +1,15 @@
 # day7 배열 만들기4
+
+def solution(arr):
+    stk = []
+    i =0
+    while i < len(arr):
+        if stk == []:
+            stk.append(arr[i])
+            i += 1
+        elif stk[-1] < arr[i]:
+            stk.append(arr[i])
+            i += 1
+        else:
+            stk.pop()
+    return stk
