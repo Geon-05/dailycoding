82a074596b29b27af5aecfe0e259f7f1d9c5913d
Geon-05 <zqrc05@gmail.com>
2024-10-28 23:46:37 +0900

    20241028

--- programmers_입문/day15/day15_2.py
+++ programmers_입문/day15/day15_2.py
@@ -1,2 +1,11 @@
 # day15 인덱스 바꾸기
 
+def solution(my_string, num1, num2):
+    answer = ''
+    my_string = list(my_string)
+    temp = my_string[num1]
+    my_string[num1] = my_string[num2]
+    my_string[num2] = temp
+    for i in my_string:
+        answer += i
+    return answer
